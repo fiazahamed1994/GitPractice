bbc1fba1dfab3f1fc1338859fc66b9d1fe68555d
My <My@DESKTOP-LSSI4SD>
2024-04-30 02:13:49 -0700

    First

---
+++ src/com/practice/Employee.java
@@ -0,0 +1,25 @@
+package com.practice;
+
+public class Employee {
+	
+	public void emp1() {
+		System.out.println("1");
+
+	}
+	
+	public void emp2() {
+		System.out.println("2");
+
+	}
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.emp1();
+	}
+	
+//	public static void main(String[] args) {
+//		Employee e = new Employee();
+//		e.emp1();
+//	}
+
+}
+
